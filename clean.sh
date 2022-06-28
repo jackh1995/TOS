e5837f5b4e78ee105933fc87bb64a1e7d3950a72
jack <pi.define.3.1415926@gmail.com>
2022-06-28 22:21:33 +0800

    gogo

--- clean.sh
+++ clean.sh
@@ -1,19 +1,12 @@
-DIR="out_dir/FTF_CE"
+DIR="out_dir/FFF"
+rm $DIR/1_400_-*.csv
+
+DIR="out_dir/FTT_CG_S"
 rm $DIR/1_400_-*.csv
 rm $DIR/2_480_-*.csv
 rm $DIR/3_400_-*.csv
 
-DIR="out_dir/FTF_CG"
-rm $DIR/1_400_-*.csv
-rm $DIR/2_480_-*.csv
-rm $DIR/3_400_-*.csv
-
-DIR="out_dir/TTT_MEAN_CG_S"
-rm $DIR/1_400_-*.csv
-rm $DIR/2_480_-*.csv
-rm $DIR/3_400_-*.csv
-
-DIR="out_dir/TTT_MEAN_CE_S"
+DIR="out_dir/TTF_MEAN_CG"
 rm $DIR/1_400_-*.csv
 rm $DIR/2_480_-*.csv
 rm $DIR/3_400_-*.csv
